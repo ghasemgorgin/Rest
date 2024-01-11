338b751f203a7a344d88333589b46bbb1afa011c
ghasemgorgin <ghasemgorgin74@gmail.com>
2024-01-11 14:59:02 +0330

    i cant create factories

--- database/factories/TeskFactory.php
+++ database/factories/TeskFactory.php
@@ -17,7 +17,8 @@ class TeskFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'title'=> $this->faker->sentence(),
+            'is done'=> false,
         ];
     }
 }
