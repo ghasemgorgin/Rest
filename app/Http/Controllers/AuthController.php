b41176ccfced252caec1c147467185a76defc285
ghasemgorgin <ghasemgorgin74@gmail.com>
2024-01-20 21:29:23 +0330

    create register controller

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -8,20 +8,35 @@ use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
-    public function login(Request $request){
-        $validate=$request->validate([
-            'email'=>'required|email',
-            'password'=>'required'
+    public function login(Request $request)
+    {
+        $validate = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required'
         ]);
-        if(!Auth::attempt($validate)){
-             return response()->json([
-                'message'=>'login failed'
-             ],401);  
+        if (!Auth::attempt($validate)) {
+            return response()->json([
+                'message' => 'login failed'
+            ], 401);
         }
-       $user=User::where('email',$validate['email'])->first();
-         return response()->json([
-            'access_token'=>$user->createToken('api_token')->plainTextToken,
-            'token_type'=>'Bearer',
-         ]);
+        $user = User::where('email', $validate['email'])->first();
+        return response()->json([
+            'access_token' => $user->createToken('api_token')->plainTextToken,
+            'token_type' => 'Bearer',
+        ]);
+    }
+    public function register(Request $request)
+    {
+        $validate = $request->validate([
+            'name' => 'required|max:255',
+            'email' => 'required|max:255|email|unique:users,email',
+            'password' => 'required|max:255|min:6|confirmed'
+        ]);
+        $user = User::create($validate);
+        return response()->json([
+            'data' => $user,
+            'access_token' => $user->createToken('api_token')->plainTextToken,
+            'token_type' => 'Bearer',
+        ], 201);
     }
 }
